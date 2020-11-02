3c450679be3bd237a16a65bdbe7a90815e7d77ab
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-02 17:49:57 -0500

    added switch case and placeholder functions to make inquirer run

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  readColleges();
+  runSearch();
 });
 
 function readColleges() {
@@ -33,3 +33,124 @@ function readColleges() {
   });
 }
 
+function runSearch() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add departments",
+        "Add roles",
+        "Add employees",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee roles",
+        "Update employee manager",
+        "Delete roles",
+        "Delete employees",
+        "Delete dapartments",
+        "View the total utilized budget of a department",
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add departments":
+        addDepartments();
+        break;
+
+      case "Add roles":
+        addRoles();
+        break;
+
+      case "Add employees":
+        addEmployees();
+        break;
+
+      case "View departments":
+        viewDepartments();
+        break;
+
+      case "View roles":
+        viewRoles();
+        break;
+
+      case "View employees":
+        viewEmployees();
+        break;
+
+      case "Update employee roles":
+        updateEmployeeRoles();
+        break;
+
+      case "Update employee manager":
+        updateEmployeeManager();
+        break;
+
+      case "Delete roles":
+        deleteRoles();
+        break;
+
+      case "Delete employees":
+        deleteEmployees();
+        break;
+
+      case "Delete Departments":
+        deleteDepartments();
+        break;
+
+      case "View the total utilized budget of a department":
+        viewBudget();
+        break;
+      }
+    });
+  }
+
+  function addDepartments(){
+
+  }
+
+  function addRoles(){
+      
+  }
+
+  function addEmployees(){
+
+}
+
+  function viewDepartments(){
+
+  }
+
+  function viewRoles() {
+
+  }
+
+  function viewEmployees() {
+
+  }
+
+  function updateEmployeeRoles(){
+
+  }
+
+  function updateEmployeeManager(){
+
+  }
+
+  function deleteRoles() {
+
+  }
+
+  function deleteEmployees(){
+
+  }
+
+  function deleteDepartments(){
+
+  }
+
+  function viewBudget(){
+
+  }
