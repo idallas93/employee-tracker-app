58a65d82ed7031984795d4087056ac5613202281
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-08 23:10:25 -0500

    updated read me

--- schema.sql
+++ schema.sql
@@ -1,32 +1,29 @@
 DROP DATABASE IF EXISTS employee_DB;
-CREATE DATABASE employee_DB;
+CREATE database employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE employees(
-	id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-    role_id INT (30) NOT NULL,
-	manager_id INT (30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-	role_id INT (30) NOT NULL,
-    title VARCHAR(30) NOT NULL,
-	salary decimal NOT NULL,
-    department_id INT (30) NOT NULL,
-    PRIMARY KEY (role_id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,4) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
-CREATE TABLE deparment (
-	department_id INT (30) NOT NULL,
-	name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (department_id)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES role(id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-values ("Isaac", "Dallas", 1, 50);
-
-SELECT * FROM employees
